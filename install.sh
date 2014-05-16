dcf2f74e4c6f6084e8f12cbc7e6404b70accbe3c
Ryo Nakamura <upa@haeena.net>
2014-05-16 22:40:55 +0900

    add veth install command to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-cp ./vxlan-pre-up /etc/network/if-pre-up.d/
-cp ./vxlan-post-down /etc/network/if-post-down.d/
+cp ./vxlan-pre-up	/etc/network/if-pre-up.d/
+cp ./vxlan-post-down	/etc/network/if-post-down.d/
+cp ./veth-pre-up	/etc/network/if-pre-up.d/
+cp ./veth-post-down	/etc/network/if-post-down.d/
